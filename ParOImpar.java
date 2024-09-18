7769932992f884c6d0382f074b793488d55962fa
Britanny_Caicedo <hiroalexander07@gmail.com>
2024-09-17 19:13:17 -0500

    Create ParOImpar.java

---
+++ ParOImpar.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ParOImpar {
+
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+        
+        System.out.print("Por favor, ingresa un número: ");
+        int numero = scanner.nextInt();
+
+        
+        if (numero % 2 == 0) {
+            System.out.println("El número " + numero + " es par.");
+        } else {
+            System.out.println("El número " + numero + " es impar.");
+        }
+
+        
+        scanner.close();
+    }
+}
